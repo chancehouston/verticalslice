746fee706434b077ccc94e331380570fde2605bc
Oliver Williams <oliver.williams@springernature.com>
2018-06-21 11:09:25 +0100

    fix cut the mustard nomodule to work

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,14 +1,6 @@
 import {treeshaketest, otherthing} from './thingy.js';
 import init from './expandable.js';
 
-import {MDCRipple} from '@material/ripple';
-
-const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
-
-// import {isEqual} from 'lodash-es';
-
-// var boo = isEqual(2, 3);
-// console.log("2 and 3 are equal is " + boo);
 var es5test = () => {
 	console.log("this arrow function will be transpiled in older browsers, and untranspiled in newer browsers!");
 }
