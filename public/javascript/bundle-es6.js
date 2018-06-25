129697402d6fc2ef067aa2553a1b63a3b615674a
Oliver Williams <oliver.williams@springernature.com>
2018-06-25 17:02:23 +0100

    define own colors rather than using global color map

--- public/javascript/bundle-es6.js
+++ public/javascript/bundle-es6.js
@@ -21,7 +21,6 @@
 		}
 	}
 
-
 	function open(box, closetext) {
 		box.querySelector('[data-expandable-box]').style.height = null;
 		box.querySelector('[data-expandable-box]').setAttribute('aria-hidden', false);
@@ -41,22 +40,21 @@
 		}
 	}
 
-	function init(boxes, height = "200px", closetext = "Show less", opentext = "Show more") {
-		boxes.forEach((box) => {
-
-			let buttonEtc = '<div class="hide-print expandable-handle js-expandable-handle hide js-show" data-test="box-expandable-handle"><div class="expandable-fade"></div><div class="strong sans-serif expandable-inner smaller-expandable-inner background-cornsilk cleared"><button type="button" aria-expanded="false" class="full-width text-left link-like sans-serif text14 strong block pl20 pt10 pb6"><span class="icon icon-right icon-rotate icon-arrow-down-12x7-blue position-relative pr15">Show more</span></button></div></div>';
+	function init(boxes, height = '200px', closetext = 'Show less', opentext = 'Show more') {
+		boxes.forEach(box => {
+			let buttonEtc = '<div><div><button type="button" aria-expanded="false"><span>Show more</span></button></div></div>';
 			box.insertAdjacentHTML('beforeEnd', buttonEtc);
 			box.isOpen = false;
 			close(box, false, height, opentext);
-			box.querySelector('button').addEventListener('click', function() {
+			box.querySelector('button').addEventListener('click', function () {
 				openOrClose(box, height, closetext, opentext);
 			});
 
-			Array.from(box.getElementsByTagName('a')).forEach(function(a) { 
-			a.addEventListener('focus', function() {
-			 open(box, closetext);   
+			Array.from(box.getElementsByTagName('a')).forEach(function (a) {
+				a.addEventListener('focus', function () {
+					open(box, closetext);
+				});
 			});
-		});
 		}
 		);
 	}
