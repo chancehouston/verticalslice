0355722025516c61ccf511a32f8f37f4367cb92c
Oliver Williams <oliver.williams@springernature.com>
2018-06-29 11:01:20 +0100

    add linting

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,28 +1,12 @@
-import {treeshaketest, otherthing} from './thingy.js';
 import init from '@springernature/global-expandable-box/js/index.js';
+import {treeshaketest, otherthing} from './thingy.js';
 
 var es5test = () => {
-	console.log("this arrow function will be transpiled in older browsers, and untranspiled in newer browsers!");
-}
+	console.log('this arrow function will be transpiled in older browsers, and untranspiled in newer browsers!');
+};
 
 var boxArray = Array.from(document.querySelectorAll('.box'));
-init(boxArray, undefined, "close", "open");
+init(boxArray, undefined, 'close', 'open');
 
 otherthing();
-
-var es5test = () => {
-	console.log("this arrow function will be transpiled in older browsers, and untranspiled in newer browsers!");
-}
-
 es5test();
-
-var box = document.querySelector('.box');
-
- box.addEventListener('open', function(e) {
-console.log('opened!!');
-})
-
-box.addEventListener('close', function() {
-	console.log('closed!!');
-})
-
